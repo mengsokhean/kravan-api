59913c4f5c38545dee7550b58a1f6befe50be501
meng sokhean <lionelheng799@gmail.com>
2026-04-30 12:35:19 +0700

    add migration route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/run-migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return "Migration finished!";
 });
