883f251ccb307fcf2e0f55dbf2643df59512dc8b
meng sokhean <lionelheng799@gmail.com>
2026-04-30 13:14:41 +0700

    security: remove migration route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
+
 Route::get('/', function () {
     return view('welcome');
-});
-Route::get('/run-migrate', function () {
-    Artisan::call('migrate', ['--force' => true]);
-    return "Migration finished!";
 });
